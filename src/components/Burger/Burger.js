5fd279c43140ecf5ff4955e8bfdf6b94bf000ecf
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2018-08-13 22:29:02 -0400

    added dynamic value in burger app

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Burger.css';
-import Ingredient from './Ingredient/Ingredient';
+import classes from "./Burger.css";
+import Ingredient from "./Ingredient/Ingredient";
 
-const burger = (props) =>{
-    const transformedIngredients = Object.keys(props.ingredients)
-    .map(key => {
-        return [...Array(props.ingredients[key])].map((_,i) => {
-           return <Ingredient key={key + i} type={key} />
-        });
-    })
-    return (
-        <div className={classes.Burger}>
-            <Ingredient type="bread-top" />
-            {transformedIngredients}
-            <Ingredient type="bread-bottom" />
-        </div>
-    );
-}
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients).map(key => {
+    return [...Array(props.ingredients[key])].map((_, i) => {
+      return <Ingredient key={key + i} type={key} />;
+    });
+  })
+  .reduce((arr,el) => {
+      return arr.concat(el);
+  }, []);
+  if(transformedIngredients.length === 0 ){
+      transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+  return (
+    <div className={classes.Burger}>
+      <Ingredient type="bread-top" />
+      {transformedIngredients}
+      <Ingredient type="bread-bottom" />
+    </div>
+  );
+};
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,25 +2,34 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     constructor(props){
         super(props);
         this.state = {
             ingredients: {
-                salad: 1,
-                bacon: 1,
-                cheese: 2,
-                meat: 2
+                salad: 0,
+                bacon: 0,
+                cheese: 0,
+                meat: 0
             }       
         }
+    }
+
+    addIngredientHandler = (type) => {
+        
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls />
             </Aux>
         );
     }
